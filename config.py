1891e7c927a5a9d5486d9e86b7e0ccf49e794beb
Jan Caha <jan.caha@lutraconsulting.co.uk>
2023-03-24 17:36:44 +0100

    let None, str or list pass the validation

--- config.py
+++ config.py
@@ -45,7 +45,6 @@ def validate_config(config):
     if config.init_from not in ["gpkg", "db"]:
         raise ConfigError(f"Config error: `init_from` parameter must be either `gpkg` or `db`. Current value is `{config.init_from}`.")
 
-    i = 0
     for conn in config.connections:
         for attr in ["driver", "conn_info", "modified", "base", "mergin_project", "sync_file"]:
             if not hasattr(conn, attr):
@@ -59,13 +58,12 @@ def validate_config(config):
 
         if "skip_tables" in conn:
             if conn.skip_tables is None:
-                pass
-                # config.update({'CONNECTIONS': [{"modified": "mergin_main"}]} {"skip_tables": []})
-            if isinstance(conn.skip_tables, str):
-                conn.update({"skip_tables": [conn.skip_tables]})
-            if not isinstance(conn.skip_tables, list):
+                continue
+            elif isinstance(conn.skip_tables, str):
+                continue
+            elif not isinstance(conn.skip_tables, list):
                 raise ConfigError("Config error: Ignored tables parameter should be a list")
-        i +=1
+
 
 def get_ignored_tables(connection):
     return connection.skip_tables if "skip_tables" in connection else []
