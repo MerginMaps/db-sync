03969d086e2a6796547d26fa8364f25b943af853
Jan Caha <jan.caha@lutraconsulting.co.uk>
2023-03-24 17:45:26 +0100

    add tests for skip_tables

--- test/test_config.py
+++ test/test_config.py
@@ -8,7 +8,7 @@ License: MIT
 import os
 import pytest
 
-from config import config, ConfigError, validate_config
+from config import config, ConfigError, validate_config, get_ignored_tables
 
 SERVER_URL = os.environ.get('TEST_MERGIN_URL')
 API_USER = os.environ.get('TEST_API_USERNAME')
@@ -64,3 +64,40 @@ def test_config():
     with pytest.raises(ConfigError, match="Config error: Name of the Mergin Maps project should be provided in the namespace/name format."):
         config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "dbsync", "sync_file": "sync.gpkg"}]})
         validate_config(config)
+
+
+def test_skip_tables():
+    _reset_config()
+
+    config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "john/dbsync", "sync_file": "sync.gpkg", "skip_tables": None}]})
+    validate_config(config)
+
+    config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "john/dbsync", "sync_file": "sync.gpkg", "skip_tables": []}]})
+    validate_config(config)
+
+    config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "john/dbsync", "sync_file": "sync.gpkg", "skip_tables": "table"}]})
+    validate_config(config)
+
+    config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "john/dbsync", "sync_file": "sync.gpkg", "skip_tables": ["table"]}]})
+
+
+def test_get_ignored_tables():
+    _reset_config()
+
+    config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "john/dbsync", "sync_file": "sync.gpkg", "skip_tables": None}]})
+    ignored_tables = get_ignored_tables(config.connections[0])
+    assert ignored_tables == []
+
+    config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "john/dbsync", "sync_file": "sync.gpkg", "skip_tables": []}]})
+    ignored_tables = get_ignored_tables(config.connections[0])
+    assert ignored_tables == []
+
+    config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "john/dbsync", "sync_file": "sync.gpkg", "skip_tables": "table"}]})
+    validate_config(config)
+    ignored_tables = get_ignored_tables(config.connections[0])
+    assert ignored_tables == ["table"]
+
+    config.update({'CONNECTIONS': [{"driver": "postgres", "conn_info": "", "modified": "mergin_main", "base": "mergin_base", "mergin_project": "john/dbsync", "sync_file": "sync.gpkg", "skip_tables": ["table"]}]})
+    validate_config(config)
+    ignored_tables = get_ignored_tables(config.connections[0])
+    assert ignored_tables == ["table"]
