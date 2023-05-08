aa9015e740072328b1fac5241a209c2d380ec798
Jan Caha <jan.caha@outlook.com>
2023-05-08 16:26:08 +0200

    Add --force option to deamon to allow clean init (#107)
    * add --force to allow complete clean
    
    * rename flag and add logical check
    
    * if from_db, drop sync file
    
    * fix sql error
    
    * add dbsync_clean tests
    
    * modify gpkg so that we can no longer push changes
    
    * update
    
    * test clean before init
    
    * update test
    
    * do more stuff with GPKG
    
    * cause problem ...
    
    * after clean everything works
    
    * update messages
    
    * fix string merge
    
    ---------
    
    Co-authored-by: Jan Caha <jan.caha@lutraconsulting.co.uk>

--- dbsync_daemon.py
+++ dbsync_daemon.py
@@ -23,6 +23,7 @@ def main():
     parser.add_argument("config_file", nargs="?", default="config.yaml", help="Path to file with configuration. Default value is config.yaml in current working directory.")
     parser.add_argument("--skip-init", action="store_true", help="Skip DB sync init step to make the tool start faster. It is not recommend to use it unless you are really sure you can skip the initial sanity checks.")
     parser.add_argument("--single-run", action="store_true", help="Run just once performing single pull and push operation, instead of running in infinite loop.")
+    parser.add_argument("--force-init", action="store_true", help="Force removing working directory and schemas from DB to initialize from scratch.")
 
     args = parser.parse_args()
 
@@ -41,8 +42,15 @@ def main():
         print("Error: " + str(e), file=sys.stderr)
         sys.exit(1)
 
+    if args.force_init and args.skip_init:
+        print("Cannot use `--force-init` with `--skip-init` Initialization is required. ", file=sys.stderr)
+        sys.exit(1)
+
     print("Logging in to Mergin...")
     mc = dbsync.create_mergin_client()
+
+    if args.force_init:
+        dbsync.dbsync_clean(mc)
 
     if args.single_run:
 
